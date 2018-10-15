58182ef6f1cd202a05a69e9a017c8ff6567ef765
Victor Arnaud <victorhad@gmail.com>
2018-10-15 01:45:40 -0300

    Criando exemplo do cálculo de IMC

---
+++ src/basico/exemplos/imc.java
@@ -0,0 +1,31 @@
+package basico.exemplos;
+
+import javax.swing.JOptionPane;
+
+/*
+ * Calula o indice de massa corporal (IMC)
+ * IMC = peso / (altura * altura) kg/m
+ */
+public class imc {
+
+    public static void main(String[] args) {
+        System.out.println("Calculo do IMC");
+
+        String entradaPeso = JOptionPane.showInputDialog("Qual o seu peso em kg?");
+        String entradaAltura = JOptionPane.showInputDialog("Qual o sua altura em metros?");
+
+        double peso = Double.parseDouble(entradaPeso);
+        double altura = Double.parseDouble(entradaAltura);
+        double imc = peso / (altura * altura);
+
+        System.out.println("IMC = " + imc + " kg/m");
+
+        String mensagem = "Fora do peso";
+        if (imc >= 20 && imc <= 25) {
+            mensagem = "Peso ideal";
+        }
+
+        JOptionPane.showMessageDialog(null, mensagem);
+
+    }
+}
