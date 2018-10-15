ca046e5b775b4ec2be91d64cee65b4cf2b4ab9da
Victor Arnaud <victorhad@gmail.com>
2018-10-14 22:48:18 -0300

    Deletando arquivos desnecessários

--- src/basico/VariaveisPrimitivas.java
+++ src/basico/VariaveisPrimitivas.java
@@ -2,7 +2,8 @@ package basico;
 
 /*
  * Apresentar os conceitos de variáveis
- * Variáveis são referências a dados e tem tipos
+ * Variável de referencia é toda aquela variável que aponta para um objeto.
+ * Variáveis primitivas são aquelas que armazenam um valor
  * Tipos primitivos:
  *  - Bytes: byte
  *  - Números inteiros: short int long
@@ -15,7 +16,6 @@ public class VariaveisPrimitivas {
 
     public static void main(String[] args) {
 
-        String titulo = "Variáveis e Sintaxe";
         char sexo = 'M'; // 16 bit
         boolean x = true; // 1 bit
         boolean y = false; // 1 bit
@@ -39,5 +39,16 @@ public class VariaveisPrimitivas {
         long n = 10000;
         c = (int) n;
         System.out.println(c);
+
+        // Variável de referencia que aponta para um objeto String.
+        String titulo = "Variáveis e Sintaxe";
+        // A partir do momento que a variável aponta para outro objeto
+        // o Garbage Collector irá remover a string acima que não está
+        // mais sendo referenciada por nenhuma variável.
+        // muito bom para liberar memória.
+        titulo = "Variáveis de referência";
+        // Para limpar a variável, ou seja, não apontar para nenhum objeto.
+        titulo = null;
+
     }
 }
