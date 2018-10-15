bdb8265a9775bae79cbd8b5bcfbf6db423197e2d
Victor Arnaud <victorhad@gmail.com>
2018-10-15 00:06:27 -0300

    Entrada de dados

---
+++ src/basico/EntradaDeDados.java
@@ -0,0 +1,25 @@
+package basico;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+/*
+ * Como recuperar informações do usuário do seu programa.
+ */
+public class EntradaDeDados {
+
+    public static void main(String[] args) {
+        // Dados passados como parâmetro (java EntradaDeDados meuArgumento1 "meu argumento 2")
+        // System.out.println(args[0]);
+
+        // Entrada de dados pelo usuário
+        System.out.print("Entre com seu nome: ");
+        Scanner entrada = new Scanner(System.in);
+        String nome = entrada.nextLine();
+        System.out.println(nome);
+
+        // Entrada de dados por uma janela
+        String sobrenome = JOptionPane.showInputDialog("Qual o seu nome?");
+        JOptionPane.showMessageDialog(null, "Bem vindo " + sobrenome);
+    }
+}
