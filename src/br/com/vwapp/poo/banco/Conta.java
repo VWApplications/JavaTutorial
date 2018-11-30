558c4a33d65968ff8359aee1b46d0e492900683c
Victor Arnaud <victorhad@gmail.com>
2018-11-30 21:35:23 -0200

    Javadoc

--- src/br/com/vwapp/poo/banco/Conta.java
+++ src/br/com/vwapp/poo/banco/Conta.java
@@ -1,48 +1,56 @@
 package br.com.vwapp.poo.banco;
 
 /**
- * ORIENTAÇÃO A OBJETOS é o paradigma de programação que mais
- * chega perto da representação do mundo real.
+ * <b>ORIENTAÇÃO A OBJETOS</b> é o paradigma de programação que mais
+ * chega perto da representação do mundo real.<br />
  *
- * CLASSES: representa o tipo de objeto que você constroi a partir dela, é como se fosse
- * um template ou modelo de criação de objetos, e esses objetos são compostos de atributos e ações.
+ * <b>CLASSES</b>: representa o tipo de objeto que você constroi a partir dela, é como se fosse
+ * um template ou modelo de criação de objetos, e esses objetos são compostos de atributos e ações.<br />
  *
- * ATRIBUTOS: são representados pelas nossas variáveis.
+ * <b>ATRIBUTOS</b>: são representados pelas nossas variáveis.<br />
  *
- * AÇÔES: são representadas pelos nossos métodos.
+ * <b>AÇÔES</b>: são representadas pelos nossos métodos.<br />
  *
- * INSTÂNCIAS: São objetos na forma concreta, por exemplo, uma classe cachorro pode ter
+ * <b>INSTÂNCIAS</b>: São objetos na forma concreta, por exemplo, uma classe cachorro pode ter
  * tamanho, nome, raça, olhos, patas, rabos, etc... e realiza ações como latir, cagar, morder,
  * e etc... com isso uma instancia dessa classe é o objeto com nome Rex de tamanho 30 Cm, raça
- * vira-lata e por ai vai.
+ * vira-lata e por ai vai.<br />
  *
- * PACOTES: estrutura de diretorios onde é colocada todas as classe de um determinado assunto, usado
+ * <b>PACOTES</b>: estrutura de diretorios onde é colocada todas as classe de um determinado assunto, usado
  * para modularizar os programas. Todos os pacotes criados por uma empresa deve ser criados com o dominio
- * da empresa invertido, por exemplo: www.xti.com.br - import br.com.xti.nomedopacote.NomeDaClasse
+ * da empresa invertido, por exemplo: www.xti.com.br - import br.com.xti.nomedopacote.NomeDaClasse<br />
  *
- * CONSTRUTORES: como o próprio nome já diz é o construtor da classe, os construtores tem o mesmo nome da classe.
+ * <b>CONSTRUTORES</b>: como o próprio nome já diz é o construtor da classe, os construtores tem o mesmo nome da classe.<br />
  *
- * MODIFICADORES DE ACESSO faz com que a visibilidade de classes, métodos e atributos seja controlado
+ * <b>MODIFICADORES DE ACESSO</b> faz com que a visibilidade de classes, métodos e atributos seja controlado<br /><br />
  *
- *      PRIVATE: visibilidade na própria classe
- *      PROTECTED: visibilidade na classe e suas subclasses
- *      PUBLIC: visibilidade total
- *      SEM MODIFICADOR: visibilidade dentro do mesmo pacote
+ * <ul>
+ *   <li><b>PRIVATE</b>: visibilidade na própria classe</li>
+ *   <li><b>PROTECTED</b>: visibilidade na classe e suas subclasses</li>
+ *   <li><b>PUBLIC</b>: visibilidade total</li>
+ *   <li><b>SEM MODIFICADOR</b>: visibilidade dentro do mesmo pacote</li>
+ *</ul><br />
  *
- * ENCAPSULAMENTO é você encapsular seus atributos como privado e criando métodos GETs e SETs para que você tenha
- * o controle de como os outros acessam e modificam seus atributos.
+ * <b>ENCAPSULAMENTO</b> é você encapsular seus atributos como privado e criando métodos GETs e SETs para que você tenha
+ * o controle de como os outros acessam e modificam seus atributos.<br />
  *
- * ATRIBUTOS E MÉTODOS ESTÁTICOS: são métodos e atributos da classe e não ao objeto da classe, por exemplo,
+ * <b>ATRIBUTOS E MÉTODOS ESTÁTICOS</b>: são métodos e atributos da classe e não ao objeto da classe, por exemplo,
  * toda vez que eu realizar um deposito ou um saque o saldo total do banco deve decrementar ou incrementar,
- * independente do objeto que realize a operação.
+ * independente do objeto que realize a operação.<br />
  *
- * CONSTANTE: para criar atributos constantes é só utilizar o final para nunca ser modificado com o static para tornar
+ * <b>CONSTANTE</b>: para criar atributos constantes é só utilizar o final para nunca ser modificado com o static para tornar
  * esse atributo único na classe. Se colocar o modificador final em uma classe ela não poderá mais ser herdada
- * e se colocar em um método ele não poderá ser modificado/sobrescrito por suas subclasses.
+ * e se colocar em um método ele não poderá ser modificado/sobrescrito por suas subclasses.<br />
  *
- * CLASSE ABSTRATA: quando você quer especificar que essa classe é apenas um template e não gera objetos,
- * não pode ser instanciada, normalmente usada quando se tem classes filhas que herdam dela.
+ * <b>CLASSE ABSTRATA</b>: quando você quer especificar que essa classe é apenas um template e não gera objetos,
+ * não pode ser instanciada, normalmente usada quando se tem classes filhas que herdam dela.<br />
  *
+ * <b>JAVADOC</b>: Documentação java - Tools -> Generate Javadoc
+ *
+ * @author victor e XTI
+ * @version 1.0
+ * @see ContaBB
+ * @see ContaITAU
  *
  */
 public abstract class Conta {
@@ -50,11 +58,12 @@ public abstract class Conta {
     // ATRIBUTOS
 
     private double saldo;
-    protected String senha;
     private static double saldoTotalDoBanco = 0.0;
     private static final int ID = 12345; // constante da classe
+    protected String senha;
 
     // CONSTRUTORES
+
     Conta() {
         this.senha = "admin1234";
         this.setSaldo(0);
