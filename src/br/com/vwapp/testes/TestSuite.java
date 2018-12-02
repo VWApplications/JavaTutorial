b5dae77821e4619e2172aaa586a6d036ca117db0
Victor Arnaud <victorhad@gmail.com>
2018-12-02 00:45:40 -0200

    Adicionando suite de teste

---
+++ src/br/com/vwapp/testes/TestSuite.java
@@ -0,0 +1,8 @@
+package br.com.vwapp.testes;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ContaTest.class})
+public class TestSuite {}
